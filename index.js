0e04c6e1aead16afc51777759d33b9cfc1ff3569
mitch kamau <michchore@gmail.com>
2024-09-24 19:28:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+function saturdayFun( activity = `roller-skate`) {
+    return (`This Saturday, I want to ${activity}!`);
+}
+function mondayWork (activity=`go to the office`) {
+    return(`This Monday, I will ${activity}.`);
+}    
+function wrapAdjective(adjective = `*`) {
+    const innerFunction = function(parameter = `special`) {
+        return (`You are ${adjective}${parameter}${adjective}!`);
+    };
+    return innerFunction
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
